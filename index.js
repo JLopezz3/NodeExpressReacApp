4842ee57ec44745248570a218d6aa85a5119ef11
JLopezz3 <lopezjerel@gmail.com>
2021-01-12 15:28:39 -0600

    update

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 const express = require("express");
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send({ hi: 'there' });
+});
+
+// listen to env PORT for production, otherwise go to 5000 for localhost. 
+const PORT = process.env.PORT || 5000;
+app.listen(PORT);
