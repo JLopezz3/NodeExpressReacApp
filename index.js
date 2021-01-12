b8116468d2f27a8f4e90f7f41655f0883ee9b366
JLopezz3 <lopezjerel@gmail.com>
2021-01-12 16:16:04 -0600

    update googleoAuth

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const express = require("express");
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send({ hi: 'there' });
-});
+// new GoogleStrategy = creates new instance of google passport strategy
+//clientID 748956231785-l68ku00tq9nc64bpcqkkrkcva8md8hg6.apps.googleusercontent.com
+// clientSecret 3_YIkgMR0eI-P0pFL7hNBAy4
+passport.use(new GoogleStrategy());
 
-// listen to env PORT for production, otherwise go to 5000 for localhost. 
+// listen to env PORT for production, otherwise go to 5000 for localhost.
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
